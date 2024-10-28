78f669fdca6e00eeb975a167ebda32b79bb69ffc
Hemanth Sai Chinthalapudi <91429511+hemanthsaich@users.noreply.github.com>
2024-10-28 12:50:01 +0530

    Update BigOExample3.java

--- BigO/BigOExample3.java
+++ BigO/BigOExample3.java
@@ -1,15 +1,15 @@
 package BigO;
 
 public class BigOExample3 {
-	
-	//O(1)
-	public static int printNum(int n) {
-		return n+n+n;
-	}
-	
-	public static void main(String[] args) {
-		int num = printNum(2);
-		System.out.println(num);
-	}
-
+    
+    // Method that performs a constant-time operation
+    // O(1) time complexity
+    public static int printNum(int n) {
+        return n + n + n; // Returns three times the value of n
+    }
+    
+    public static void main(String[] args) {
+        int num = printNum(2); // Calling the method with n = 2
+        System.out.println(num); // Output: 6
+    }
 }
