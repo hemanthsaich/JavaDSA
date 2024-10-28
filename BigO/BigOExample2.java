2af8d9878c56388ad68d522d3c1315f6013193dc
Hemanth Sai Chinthalapudi <91429511+hemanthsaich@users.noreply.github.com>
2024-10-28 12:49:46 +0530

    Update BigOExample2.java

--- BigO/BigOExample2.java
+++ BigO/BigOExample2.java
@@ -1,18 +1,19 @@
 package BigO;
 
 public class BigOExample2 {
-	
-	// O(n^2)
-	public static void printNum(int n) {
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<n ; j++) {	
-				System.out.println(i +" "+j);
-			}
-		}
-	}
-	
-	public static void main(String[] args) {
-		printNum(10);
-	}
-
+    
+    // Method to print pairs (i, j) for each i and j from 0 to n-1
+    // This has O(n^2) time complexity
+    public static void printNum(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {    
+                System.out.println(i + " " + j); // O(1) operation inside the inner loop
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        // Calling the method with n = 10
+        printNum(10);
+    }
 }
