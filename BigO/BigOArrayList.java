a00a0a86bdcfa7d03ada3da9e69b5a89f3c3fd1b
Hemanth Sai Chinthalapudi <91429511+hemanthsaich@users.noreply.github.com>
2024-10-28 12:48:58 +0530

    Update BigOArrayList.java

--- BigO/BigOArrayList.java
+++ BigO/BigOArrayList.java
@@ -1,34 +1,34 @@
 package BigO;
 
 import java.util.ArrayList;
+
 public class BigOArrayList {
-	
-	public static void main(String[] args) {
-		
-		//Creating ArrayList
-		ArrayList<Integer> myList = new ArrayList<>();
-		
-		//Adding elements
-		
-		myList.add(10);
-		myList.add(20);
-		myList.add(30);
-		System.out.println(myList);
-		
-		// O(1) 
-		myList.add(40);
-		System.out.println("40 added at index 3"+myList);
-		myList.remove(3);
-		System.out.println("element removed at index 3"+myList);
-		
-		//O(n)
-		myList.remove("element removed at index 0"+0);
-		System.out.println(myList);
-		myList.add(0, 10);
-		System.out.println("0 added at index 0"+myList);
-
-		
-	}
-	
-
+    
+    public static void main(String[] args) {
+        
+        // Creating an ArrayList
+        ArrayList<Integer> myList = new ArrayList<>();
+        
+        // Adding elements to the ArrayList
+        myList.add(10);
+        myList.add(20);
+        myList.add(30);
+        System.out.println("Initial List: " + myList);
+        
+        // O(1) - Adding an element
+        myList.add(40);
+        System.out.println("After adding 40 at index 3: " + myList);
+        
+        // O(1) - Removing an element
+        myList.remove(3);
+        System.out.println("After removing element at index 3: " + myList);
+        
+        // O(n) - Removing the first element (shifts elements)
+        myList.remove(0);
+        System.out.println("After removing element at index 0: " + myList);
+        
+        // O(n) - Adding an element at index 0 (shifts elements)
+        myList.add(0, 10);
+        System.out.println("After adding 10 at index 0: " + myList);
+    }
 }
