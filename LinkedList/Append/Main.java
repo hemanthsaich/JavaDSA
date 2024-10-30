ddbc3af5b54fb7f0f25ae575e88ad3e3a1d9ae61
Hemanth Sai Chinthalapudi <91429511+hemanthsaich@users.noreply.github.com>
2024-10-30 17:50:34 +0530

    LinkedList Append

---
+++ LinkedList/Append/Main.java
@@ -0,0 +1,16 @@
+package LinkedList.Append;
+
+public class Main {
+	public static void main(String[] args) {
+		LinkedList linkedList = new LinkedList(4);
+		
+		
+		linkedList.append(2);
+		linkedList.append(3);
+
+		
+		linkedList.printList();
+	}
+}
+
+
